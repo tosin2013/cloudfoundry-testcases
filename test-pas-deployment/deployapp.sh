026a1b35b977f65fc10d66cd20e8d715851496ca
Tosin Akinosho <oluwatosin.akinosho@altoros.com>
2018-10-24 11:12:55 -0700

    added cf login to test-pas-deployment/deployapp.sh

--- test-pas-deployment/deployapp.sh
+++ test-pas-deployment/deployapp.sh
@@ -1,6 +1,16 @@
 #/bin/bash
+set -xe
 
+if [[ -z $1 ]]; then
+    echo "Please pass enviornment file to be used during deployment."
+    echo "USAGE: deployapp.sh $1"
+    exit $?
+fi
 
 git clone https://github.com/cloudfoundry-samples/spring-music
 
 cd spring-music
+
+./gradlew clean assemble
+
+cf login -a https://${PCFURL} -u ${USERNAME} -p ${PASSWORD}
